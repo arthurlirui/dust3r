374e8d603e964e6af8eca808320afd5970407283
Rui Li <rui.li@kaust.edu.sa>
2024-08-25 15:43:10 +0800

    add dual camera support

--- dust3r/utils/image.py
+++ dust3r/utils/image.py
@@ -70,7 +70,14 @@ def _resize_pil_image(img, long_edge_size):
     return img.resize(new_size, interp)
 
 
-def load_images(folder_or_list, size, square_ok=False, verbose=True):
+def split_img(img_path):
+    img = cv2.imread(img_path)
+    img0 = img[:, :1920, :]
+    img1 = img[:, 1920:, :]
+    return img0, img1
+
+
+def load_images(folder_or_list, size, square_ok=False, dual_camera=False, verbose=True):
     """ open and convert all images in a list or folder to proper input format for DUSt3R
     """
     if isinstance(folder_or_list, str):
@@ -97,28 +104,37 @@ def load_images(folder_or_list, size, square_ok=False, verbose=True):
             continue
         img = exif_transpose(PIL.Image.open(os.path.join(root, path))).convert('RGB')
         W1, H1 = img.size
-        if size == 224:
-            # resize short side to 224 (then crop)
-            img = _resize_pil_image(img, round(size * max(W1/H1, H1/W1)))
+        if dual_camera:
+            img0 = PIL.Image.fromarray(np.array(img)[:, :W1//2, :])
+            img1 = PIL.Image.fromarray(np.array(img)[:, W1//2:, :])
+            img_split = [img0, img1]
+            W1, H1 = img0.size
         else:
-            # resize long side to 512
-            img = _resize_pil_image(img, size)
-        W, H = img.size
-        cx, cy = W//2, H//2
-        if size == 224:
-            half = min(cx, cy)
-            img = img.crop((cx-half, cy-half, cx+half, cy+half))
-        else:
-            halfw, halfh = ((2*cx)//16)*8, ((2*cy)//16)*8
-            if not (square_ok) and W == H:
-                halfh = 3*halfw/4
-            img = img.crop((cx-halfw, cy-halfh, cx+halfw, cy+halfh))
+            img_split = [img]
 
-        W2, H2 = img.size
-        if verbose:
-            print(f' - adding {path} with resolution {W1}x{H1} --> {W2}x{H2}')
-        imgs.append(dict(img=ImgNorm(img)[None], true_shape=np.int32(
-            [img.size[::-1]]), idx=len(imgs), instance=str(len(imgs))))
+        for imgt in img_split:
+            if size == 224:
+                # resize short side to 224 (then crop)
+                img = _resize_pil_image(imgt, round(size * max(W1/H1, H1/W1)))
+            else:
+                # resize long side to 512
+                img = _resize_pil_image(imgt, size)
+            W, H = img.size
+            cx, cy = W//2, H//2
+            if size == 224:
+                half = min(cx, cy)
+                img = img.crop((cx-half, cy-half, cx+half, cy+half))
+            else:
+                halfw, halfh = ((2*cx)//16)*8, ((2*cy)//16)*8
+                if not (square_ok) and W == H:
+                    halfh = 3*halfw/4
+                img = img.crop((cx-halfw, cy-halfh, cx+halfw, cy+halfh))
+
+            W2, H2 = img.size
+            if verbose:
+                print(f' - adding {path} with resolution {W1}x{H1} --> {W2}x{H2}')
+            imgs.append(dict(img=ImgNorm(img)[None], true_shape=np.int32(
+                [img.size[::-1]]), idx=len(imgs), instance=str(len(imgs))))
 
     assert imgs, 'no images foud at '+root
     if verbose:
